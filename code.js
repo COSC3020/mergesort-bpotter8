3c56b455085436a95edb90b7ea6db6cd3a154653
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-03-25 22:27:45 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,36 +1,35 @@
 function mergesort(array) {
     const n = array.length;
     
-    for(let size = 1; size < n; size *= 2) {
-        for(let left = 0; left < n - 1; left += size * 2) {
+    for (let size = 1; size < n; size *= 2) {
+        for (let left = 0; left < n - 1; left += 2 * size) {
             const mid = Math.min(left + size - 1, n - 1);
             const right = Math.min(left + 2 * size - 1, n - 1);
             mergeArr (array, left, mid, right);
         }
     }
+    
     return array;
 }
 
-function mergeArr(arr, l, m, r){
-    let i = 0;
-    let j = 0;
-    let k = l;
-    const leftHalf = arr.slice(l, m + 1);
-    const rightHalf = arr.slice(m + 1, r + 1);
+function mergeArr(arr, l, m, r) {
+    let i = l;
+    let j = m + 1;
 
-    while (i < leftHalf.length && j < rightHalf.length){
-        if (leftHalf[i] <= rightHalf[j]){
-            arr[k++] = leftHalf[i++];
+    while (i <= m && j <= r) {
+        if (arr[i] <= arr[j]) {
+            i++
         } else {
-            arr[k++] = rightHalf[j++];
+            const t = arr[j];
+            
+            for(let k = j; k > i; k--){
+                arr[k] = arr[k - 1];
+            }
+            
+            arr[i] = t;
+            i++
+            j++
+            m++
         }
-    }
-
-    while (i < leftHalf.length){
-        arr[k++] = leftHalf[i++];
-    }
-
-    while (j < rightHalf.length){
-        arr[k++] = rightHalf[j++];
     }
 }
